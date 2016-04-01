ad829f927ef53102bd0d6baba420aeba943a9cee
YUE LI <nicholeli4129@gmail.com>
2016-04-01 15:00:28 -0400

    ..

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,8 +19,10 @@ public class Main {
 
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
-    
+
     Object r1 = new Userinfo();
+    Object r2 = new SignUpServer();
+   
 
     get("/hello", (req, res) -> "Hello World");
 
