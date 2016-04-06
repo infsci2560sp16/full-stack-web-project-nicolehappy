ffc945303fa8da2d1ab35bd4f261819e6029a9fd
YUE LI <nicholeli4129@gmail.com>
2016-04-06 18:41:28 -0400

    modify..

---
+++ src/main/java/ItemService.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LikeService {
+	private List<Item> items = new ArrayList<Item>();
+	ItemService(){
+		items.add(new Item("1","powders"));
+		items.add(new Item("2","lips"));
+	}
+}
